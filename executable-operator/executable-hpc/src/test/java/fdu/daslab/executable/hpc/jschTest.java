d77dc72113c8a565aa4974a591eed82e0722d074
zjchenn <67681132+zjchenn@users.noreply.github.com>
2021-10-11 10:21:10 +0800

    fix(hpc): change to manually input the username & password of cluster

--- executable-operator/executable-hpc/src/test/java/fdu/daslab/executable/hpc/jschTest.java
+++ executable-operator/executable-hpc/src/test/java/fdu/daslab/executable/hpc/jschTest.java
@@ -77,8 +77,8 @@
 //
 //            // run a shell script
 ////            channelExec.setCommand("echo $PWD; source /es01/shanhe/hpc_mnt/home/usr-dDyDTogd/.bashrc; /opt/skyformai/bin/csub -n 56 -cwd /es01/shanhe/hpc_mnt/src/COAWST_v1467/Projects/JOE_TC/Coupled ./joe_tc.aip.slurm");
-//            channelExec.setCommand("source /es01/shanhe/hpc_mnt/home/usr-dDyDTogd/.bashrc; /opt/skyformai/bin/cjobs");
-////            channelExec.setCommand(SCRIPT_PATH + " WRF 3 2 2");
+////            channelExec.setCommand("source /es01/shanhe/hpc_mnt/home/usr-dDyDTogd/.bashrc; /opt/skyformai/bin/cjobs");
+//            channelExec.setCommand(SCRIPT_PATH + " WRF 3 2 2;" + SCRIPT_PATH + " MIX 2 2");
 //
 //
 //            // display errors to System.err
